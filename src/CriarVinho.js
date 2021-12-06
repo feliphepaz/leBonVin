b7d5a5ba7afc3a044eeeba133400cb3ffb2d1dd9
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-12-06 15:24:35 -0300

    progresso na pagina de criar vinho

--- src/CriarVinho.js
+++ src/CriarVinho.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import './CriarVinho.css';
 
 const CriarVinho = () => {
+    const [userID, setUserID] = React.useState('');
+    const [wines, setWines] = React.useState([]);
+
+    React.useEffect(() => {
+        getUserID();
+        userWines();
+    }, [userID])
+
+    async function getUserID() {
+        const response = await fetch('http://lebonvin.local/json/api/user', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + window.localStorage.getItem('Token'),
+            },
+        });
+        const resolve = await response.json();
+        setUserID(resolve.id)
+    }
+
+    async function userWines() {
+        const response = await fetch(`http://lebonvin.local/json/api/wine/?_user=${userID}`);
+        const resolve = await response.json();
+        setWines(resolve);
+    }
+
+
     const [nome, setNome] = React.useState('');
     const [codigo, setCodigo] = React.useState('');
     const [ano, setAno] = React.useState('');
@@ -63,29 +90,36 @@ const CriarVinho = () => {
 
 
     return (
-        <>
-        <form className='form criar-vinho-form' onSubmit={handleForm}>
-            <input type='text' placeholder='Nome do Vinho' value={nome} onChange={({target}) => setNome(target.value)} />
-            <input type='text' placeholder='Código' value={codigo} onChange={({target}) => setCodigo(target.value)} />
-            <input type='text' placeholder='Ano' value={ano} onChange={({target}) => setAno(target.value)} />
-            <input type='text' placeholder='Quantidade' value={quantidade} onChange={({target}) => setQuantidade(target.value)} />
-            <input type='text' placeholder='Tipo' value={tipo} onChange={({target}) => setTipo(target.value)} />
-            <input type='text' placeholder='País' value={pais} onChange={({target}) => setPais(target.value)} />
-            <textarea placeholder='Descrição' value={descricao} onChange={({target}) => setDescricao(target.value)} />
-            <input type='text' placeholder='Uva' value={uva} onChange={({target}) => setUva(target.value)} />
-            <input type='text' placeholder='Amadurecimento' value={amadurecimento} onChange={({target}) => setAmadurecimento(target.value)} />
-            <input type='text' placeholder='Região' value={regiao} onChange={({target}) => setRegiao(target.value)} />
-            <input type='text' placeholder='Classificação' value={classificacao} onChange={({target}) => setClassificacao(target.value)} />
-            <input type='text' placeholder='Teor' value={teor} onChange={({target}) => setTeor(target.value)} />
-            <input type='text' placeholder='Potencial' value={potencial} onChange={({target}) => setPotencial(target.value)} />
-            <input type='text' placeholder='Temperatura' value={temperatura} onChange={({target}) => setTemperatura(target.value)} />
-            <input type='file' name='img' id='img' onChange={handleImgForm} />
-            <input className='submit' type='submit' value={loading ? 'Criando...' : 'Criar Vinho'} disabled={loading && 'disabled'} />
-        </form>
-        <div>
-            {img.preview && <div className='img-preview' style={{backgroundImage: `url('${img.preview}')`}}></div>}
-        </div>
-        </>
+        <section className='criar-vinho'>
+            <div className='criar-vinho-1'>
+                <form className='form criar-vinho-form' onSubmit={handleForm}>
+                    <input type='text' placeholder='Nome do Vinho' value={nome} onChange={({target}) => setNome(target.value)} />
+                    <input type='text' placeholder='Código' value={codigo} onChange={({target}) => setCodigo(target.value)} />
+                    <input type='text' placeholder='Ano' value={ano} onChange={({target}) => setAno(target.value)} />
+                    <input type='text' placeholder='Quantidade' value={quantidade} onChange={({target}) => setQuantidade(target.value)} />
+                    <input type='text' placeholder='Tipo' value={tipo} onChange={({target}) => setTipo(target.value)} />
+                    <input type='text' placeholder='País' value={pais} onChange={({target}) => setPais(target.value)} />
+                    <textarea placeholder='Descrição' value={descricao} onChange={({target}) => setDescricao(target.value)} />
+                    <input type='text' placeholder='Uva' value={uva} onChange={({target}) => setUva(target.value)} />
+                    <input type='text' placeholder='Amadurecimento' value={amadurecimento} onChange={({target}) => setAmadurecimento(target.value)} />
+                    <input type='text' placeholder='Região' value={regiao} onChange={({target}) => setRegiao(target.value)} />
+                    <input type='text' placeholder='Classificação' value={classificacao} onChange={({target}) => setClassificacao(target.value)} />
+                    <input type='text' placeholder='Teor' value={teor} onChange={({target}) => setTeor(target.value)} />
+                    <input type='text' placeholder='Potencial' value={potencial} onChange={({target}) => setPotencial(target.value)} />
+                    <input type='text' placeholder='Temperatura' value={temperatura} onChange={({target}) => setTemperatura(target.value)} />
+                    <input type='file' name='img' id='img' onChange={handleImgForm} />
+                    <input className='submit' type='submit' value={loading ? 'Criando...' : 'Criar Vinho'} disabled={loading && 'disabled'} />
+                </form>
+                <div>
+                    {img.preview && <div className='img-preview' style={{backgroundImage: `url('${img.preview}')`}}></div>}
+                </div>
+            </div>
+            <ul className='criar-vinho-2'>
+                {wines.map((wine) => (
+                    <li key={wine.id}>{wine.title}</li>
+                ))}
+            </ul>
+        </section>
     )
 }
 
